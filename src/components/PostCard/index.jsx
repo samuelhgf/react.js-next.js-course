9f7b013aa05238fec99ea433c36904ee7b49e677
Samuel França <samuelhenriques2000@gmail.com>
2023-05-17 21:49:19 -0300

    Fix alt property

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,8 +1,8 @@
 import "./styles.css"
 
 export const PostCard = ({ id, cover, title, body }) => (
-    <div className='post' alt={title}>
-        <img src={cover}></img>
+    <div className='post'>
+        <img src={cover} alt={title}></img>
         <div className='post-content'>
             <h1>{title}</h1>
             <p>{body}</p>
